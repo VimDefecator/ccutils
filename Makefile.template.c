dfc49e042e3a59744cae943c5893b3f95500432d
Alexander Polazhinets <polazhinets.a@gmail.com>
2024-03-24 14:19:26 +0100

    add makefile template for C

---
+++ Makefile.template.c
@@ -0,0 +1,30 @@
+define TARGET_template =
+ OBJS_$(1) := $$(foreach name,$(2),build/$$(name).o)
+ build/$(1): $$(OBJS_$(1))
+	$(CC) -o build/$(1) $$(OBJS_$(1)) $(LDFLAGS) $(LDFLAGS_$(1))
+endef
+
+all: $(TARGETS:%=build/%)
+
+$(foreach target,$(TARGETS),\
+	$(eval $(call TARGET_template,$(target),$(UNITS_$(target)))))
+
+$(foreach target,$(TARGETS),\
+	$(foreach unit,$(UNITS_$(target)),\
+		$(eval ALLUNITS := $(filter-out $(unit),$(ALLUNITS)) $(unit))))
+
+$(ALLUNITS:%=build/%.o): build/%.o: src/%.c build/.dir
+	$(CC) $(CFLAGS) $(CPPFLAGS) -c $< -o $@
+
+$(foreach dir,$(foreach unit,$(ALLUNITS),$(shell dirname build/$(unit))),\
+	$(eval ALLDIRS := $(filter-out $(dir),$(ALLDIRS)) $(dir)))
+
+build/.dir:
+	mkdir -p $(ALLDIRS)
+	touch build/.dir
+
+.PHONY: clean
+clean:
+	rm -rf build
+
+-include $(ALLUNITS:%=build/%.d)
